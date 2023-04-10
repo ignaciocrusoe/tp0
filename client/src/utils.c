25b9eeda2f6503f32e60e8928989452b0981d5e2
Ignacio Crusoe <icrusoe@frba.utn.edu.ar>
2023-04-10 00:15:43 +0000

    Conexión cliente-servidor

--- client/src/utils.c
+++ client/src/utils.c
@@ -30,7 +30,7 @@ int crear_conexion(char *ip, char* puerto)
 
 	// Ahora vamos a crear el socket.
 	int socket_cliente = 0;
-
+	socket_cliente = socket(server_info->ai_family, server_info->ai_socktype, server_info->ai_protocol);
 	//------------------------------------
 	printf("crear_conexion: IP: %s - PUERTO: %s\n", ip, puerto);
 	// Ahora que tenemos el socket, vamos a conectarlo
